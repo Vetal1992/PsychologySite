63a3294b5e9824092446ce15953c2a085ea043c8
Vitaliy_Chernish <71778798+Vetal1992@users.noreply.github.com>
2021-11-29 22:21:40 +0300

    Update TextAppointment.jsx

--- src/Components/navbar/appointment/TextAppointment.jsx
+++ src/Components/navbar/appointment/TextAppointment.jsx
@@ -5,7 +5,7 @@ const TextAppointment = () => {
     return (
         <div className={styles.item} >
             <div className={styles.appointmentPage} id='textAppointment' style={{ visibility: 'hidden', fontSize: '1vw' }} href="">
-                <p>Some fucking text for Appointment </p>
+                <p>Some text for Appointment </p>
                 <p>Some text for cool psycholog in Appointment</p>
                 <p>Some text for something Appointment</p>
             </div>
