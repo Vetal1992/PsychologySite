e20373e2697132322c36c29de2a294d26bc1fdd3
Vitaliy_Chernish <71778798+Vetal1992@users.noreply.github.com>
2021-11-29 22:22:33 +0300

    Update TextContacts.jsx

--- src/Components/navbar/contacts/TextContacts.jsx
+++ src/Components/navbar/contacts/TextContacts.jsx
@@ -5,7 +5,7 @@ const TextContacts = () => {
     return (
         <div className={styles.item} >
             <div className={styles.contactsPage} id='textContacts' style={{ visibility: 'hidden', fontSize: '1vw' }} href="">
-                <p>Some fucking text for Contacts </p>
+                <p>Some text for Contacts </p>
                 <p>Some text for cool psycholog in Contacts</p>
                 <p>Some text for something Contacts</p>
             </div>
