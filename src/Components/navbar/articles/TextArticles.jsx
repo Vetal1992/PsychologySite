dace5ec7294f6b268c1ce9d798813f08bdf2bb2e
Vitaliy_Chernish <71778798+Vetal1992@users.noreply.github.com>
2021-11-29 22:22:08 +0300

    Update TextArticles.jsx

--- src/Components/navbar/articles/TextArticles.jsx
+++ src/Components/navbar/articles/TextArticles.jsx
@@ -5,7 +5,7 @@ const TextArticles = () => {
     return (
         <div className={styles.item} >
             <div className={styles.articlesPage} id='textArticles' style={{ visibility: 'hidden', fontSize: '1vw' }} href="">
-                <p>Some fucking text for Articles </p>
+                <p>Some text for Articles </p>
                 <p>Some text for cool psycholog in Articles</p>
                 <p>Some text for something Articles</p>
             </div>
